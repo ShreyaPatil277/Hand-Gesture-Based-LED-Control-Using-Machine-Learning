677224c09cb817aea830e439cb89b3a3e793d8d7
ShreyaPatil277 <shreyagpatil2005@gmail.com>
2025-07-25 23:11:30 +0530

    controller.py

--- controller.py
+++ controller.py
@@ -1,18 +1,79 @@
-
 import pyfirmata
 
-comport = 'COM5'  # Change to your actual COM port
-board = pyfirmata.Arduino(comport)
+comport='COM5'
 
-# Define LED pins (digital 8–12)
-led_pins = [board.get_pin(f'd:{i}:o') for i in [8, 9, 10, 11, 12]]
+board=pyfirmata.Arduino(comport)
+
+led_1=board.get_pin('d:8:o')
+led_2=board.get_pin('d:9:o')
+led_3=board.get_pin('d:10:o')
+led_4=board.get_pin('d:11:o')
+led_5=board.get_pin('d:12:o')
 
 def led(fingerUp):
-    # Safely handle if fingerUp is not 5 elements
-    if len(fingerUp) != 5:
-        print("Invalid finger pattern:", fingerUp)
-        return
-    
-    print("Finger Pattern:", fingerUp)
-    for i in range(5):
-        led_pins[i].write(fingerUp[i])
+    if fingerUp == [0, 0, 0, 0, 0]:
+        led_1.write(0); led_2.write(0); led_3.write(0); led_4.write(0); led_5.write(0)
+    elif fingerUp == [0, 0, 0, 0, 1]:
+        led_1.write(0); led_2.write(0); led_3.write(0); led_4.write(0); led_5.write(1)
+    elif fingerUp == [0, 0, 0, 1, 0]:
+        led_1.write(0); led_2.write(0); led_3.write(0); led_4.write(1); led_5.write(0)
+    elif fingerUp == [0, 0, 0, 1, 1]:
+        led_1.write(0); led_2.write(0); led_3.write(0); led_4.write(1); led_5.write(1)
+    elif fingerUp == [0, 0, 1, 0, 0]:
+        led_1.write(0); led_2.write(0); led_3.write(1); led_4.write(0); led_5.write(0)
+    elif fingerUp == [0, 0, 1, 0, 1]:
+        led_1.write(0); led_2.write(0); led_3.write(1); led_4.write(0); led_5.write(1)
+    elif fingerUp == [0, 0, 1, 1, 0]:
+        led_1.write(0); led_2.write(0); led_3.write(1); led_4.write(1); led_5.write(0)
+    elif fingerUp == [0, 0, 1, 1, 1]:
+        led_1.write(0); led_2.write(0); led_3.write(1); led_4.write(1); led_5.write(1)
+    elif fingerUp == [0, 1, 0, 0, 0]:
+        led_1.write(0); led_2.write(1); led_3.write(0); led_4.write(0); led_5.write(0)
+    elif fingerUp == [0, 1, 0, 0, 1]:
+        led_1.write(0); led_2.write(1); led_3.write(0); led_4.write(0); led_5.write(1)
+    elif fingerUp == [0, 1, 0, 1, 0]:
+        led_1.write(0); led_2.write(1); led_3.write(0); led_4.write(1); led_5.write(0)
+    elif fingerUp == [0, 1, 0, 1, 1]:
+        led_1.write(0); led_2.write(1); led_3.write(0); led_4.write(1); led_5.write(1)
+    elif fingerUp == [0, 1, 1, 0, 0]:
+        led_1.write(0); led_2.write(1); led_3.write(1); led_4.write(0); led_5.write(0)
+    elif fingerUp == [0, 1, 1, 0, 1]:
+        led_1.write(0); led_2.write(1); led_3.write(1); led_4.write(0); led_5.write(1)
+    elif fingerUp == [0, 1, 1, 1, 0]:
+        led_1.write(0); led_2.write(1); led_3.write(1); led_4.write(1); led_5.write(0)
+    elif fingerUp == [0, 1, 1, 1, 1]:
+        led_1.write(0); led_2.write(1); led_3.write(1); led_4.write(1); led_5.write(1)
+    elif fingerUp == [1, 0, 0, 0, 0]:
+        led_1.write(1); led_2.write(0); led_3.write(0); led_4.write(0); led_5.write(0)
+    elif fingerUp == [1, 0, 0, 0, 1]:
+        led_1.write(1); led_2.write(0); led_3.write(0); led_4.write(0); led_5.write(1)
+    elif fingerUp == [1, 0, 0, 1, 0]:
+        led_1.write(1); led_2.write(0); led_3.write(0); led_4.write(1); led_5.write(0)
+    elif fingerUp == [1, 0, 0, 1, 1]:
+        led_1.write(1); led_2.write(0); led_3.write(0); led_4.write(1); led_5.write(1)
+    elif fingerUp == [1, 0, 1, 0, 0]:
+        led_1.write(1); led_2.write(0); led_3.write(1); led_4.write(0); led_5.write(0)
+    elif fingerUp == [1, 0, 1, 0, 1]:
+        led_1.write(1); led_2.write(0); led_3.write(1); led_4.write(0); led_5.write(1)
+    elif fingerUp == [1, 0, 1, 1, 0]:
+        led_1.write(1); led_2.write(0); led_3.write(1); led_4.write(1); led_5.write(0)
+    elif fingerUp == [1, 0, 1, 1, 1]:
+        led_1.write(1); led_2.write(0); led_3.write(1); led_4.write(1); led_5.write(1)
+    elif fingerUp == [1, 1, 0, 0, 0]:
+        led_1.write(1); led_2.write(1); led_3.write(0); led_4.write(0); led_5.write(0)
+    elif fingerUp == [1, 1, 0, 0, 1]:
+        led_1.write(1); led_2.write(1); led_3.write(0); led_4.write(0); led_5.write(1)
+    elif fingerUp == [1, 1, 0, 1, 0]:
+        led_1.write(1); led_2.write(1); led_3.write(0); led_4.write(1); led_5.write(0)
+    elif fingerUp == [1, 1, 0, 1, 1]:
+        led_1.write(1); led_2.write(1); led_3.write(0); led_4.write(1); led_5.write(1)
+    elif fingerUp == [1, 1, 1, 0, 0]:
+        led_1.write(1); led_2.write(1); led_3.write(1); led_4.write(0); led_5.write(0)
+    elif fingerUp == [1, 1, 1, 0, 1]:
+        led_1.write(1); led_2.write(1); led_3.write(1); led_4.write(0); led_5.write(1)
+    elif fingerUp == [1, 1, 1, 1, 0]:
+        led_1.write(1); led_2.write(1); led_3.write(1); led_4.write(1); led_5.write(0)
+    elif fingerUp == [1, 1, 1, 1, 1]:
+        led_1.write(1); led_2.write(1); led_3.write(1); led_4.write(1); led_5.write(1)
+
+
